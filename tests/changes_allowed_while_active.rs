45bd110211d0a8c8659ba2261b7ea0fd35164671
Diego Barrios Romero <eldruin@gmail.com>
2020-05-23 11:45:18 +0200

    Test changes allowed while active

---
+++ tests/changes_allowed_while_active.rs
@@ -0,0 +1,25 @@
+mod base;
+use crate::base::{destroy, new_mma8451, new_mma8652, Register, ADDRESS};
+use embedded_hal_mock::i2c::Transaction as I2cTrans;
+
+macro_rules! tests {
+    ($name:ident, $create:ident) => {
+        mod $name {
+            use super::*;
+
+            #[test]
+            fn set_debounce_counter() {
+                let sensor = $create(&[
+                    I2cTrans::write(ADDRESS, vec![Register::CTRL_REG1, 1]),
+                    I2cTrans::write(ADDRESS, vec![Register::PL_COUNT, 0xAB]),
+                ]);
+                let mut sensor = sensor.active().ok().unwrap();
+                sensor.set_debounce_counter(0xAB).unwrap();
+                destroy(sensor);
+            }
+        }
+    };
+}
+
+tests!(mma8451, new_mma8451);
+tests!(mma8652, new_mma8652);
