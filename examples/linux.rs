c8e019bf5766bf3d915c1634e274bb363684c932
Diego Barrios Romero <eldruin@gmail.com>
2020-05-24 16:21:14 +0200

    Improve example

--- examples/linux.rs
+++ examples/linux.rs
@@ -3,9 +3,10 @@ use mma8x5x::Mma8x5x;
 
 fn main() {
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
-    let mut sensor = Mma8x5x::new_mma8653(dev);
+    let sensor = Mma8x5x::new_mma8653(dev);
+    let mut sensor = sensor.into_active().ok().unwrap();
     loop {
-        let id = sensor.device_id().unwrap_or(0);
-        println!("Device ID: 0x{:x}", id);
+        let accel = sensor.read().unwrap();
+        println!("Acceleration: {:?}", accel);
     }
 }
